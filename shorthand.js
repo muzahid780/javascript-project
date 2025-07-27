f2b45b7e16718face1e563147297f52eecc42577
Md.Muzahidur Rahman <muzahidur780@gmail.com>
2025-07-27 13:40:06 +0600

    added operators and addition, subtraction, multiplication and concat

---
+++ shorthand.js
@@ -0,0 +1,30 @@
+//module-13
+
+// addition
+var price = 35;
+// price = price+10;
+price += 10;
+console.log(price);
+
+// subtraction
+var fridge = 35000;
+fridge -= 1000;
+console.log(fridge);
+
+// multiply
+var orangePrice = 30;
+orangePrice *= 3;
+console.log(orangePrice);
+
+//division
+
+// mixed-concat
+var first = "Happy";
+var second = "Coding";
+
+var fullName = first + second;
+console.log(fullName);
+
+console.log("4" + 20);
+console.log(4 - "20");
+console.log("40" - 20);
