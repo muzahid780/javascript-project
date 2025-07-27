3e3ae4e877c133d43aa8f5b34c7b177dc312d57a
Md.Muzahidur Rahman <muzahidur780@gmail.com>
2025-07-27 12:42:36 +0600

    added variable naming rules

--- variable.js
+++ variable.js
@@ -1,3 +1,40 @@
+//number
 var age = 24;
 var price = 250;
 console.log(price);
+
+// string
+var name = "Muzahid";
+var country = "Bangladesh";
+console.log(name);
+console.log(country);
+
+//boolean
+var isRich = true;
+var passed = false;
+console.log(isRich);
+console.log(passed);
+
+//typeof
+console.log(typeof passed);
+console.log(typeof country);
+console.log(typeof price);
+
+// variable naming rules,,
+// 1.no keyword in variable name>>var false = 35;
+
+// 2.no space or gap in variable name>>var is space = false;
+
+// 3.no quotation in variable name>> var "address"="kochu khet";
+// 4.can not start with a number>> var 3money = 45;
+// but number other than the first letter is allowed>>
+var money2 = 100;
+// 5.name is case-sensitive>>
+address;
+Address;
+ADDRESS;
+address;
+//6.how to write a long variable name
+var my_current_home_address = "sundorban kata bon"; // snake case
+var myCurrentHomeAddress = "sundorban kata bon"; // camel case
+var MyCurrentHomeAddress = "sundorban kata bon"; // pascal case
