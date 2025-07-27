546c2786a76617226fdeb56b0e11ccd7a22e8a03
Md.Muzahidur Rahman <muzahidur780@gmail.com>
2025-07-27 13:09:46 +0600

    added types and convert string to integer and float number

---
+++ number.js
@@ -0,0 +1,18 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = 25;
+var applePrice = parseInt("30.5"); //string>>integer
+var applePrice = parseFloat("30.5"); //string>>float
+
+console.log(typeof orangePrice);
+console.log(applePrice);
+console.log(orangePrice + applePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total);
+console.log(total.toFixed(2));
+
+const num = 50 / 0;
+console.log(num);
